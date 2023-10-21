cb1f8c590ad5549b39054f1584cc2a8592a07c92
RCJayalath <95970074+RCJayalath@users.noreply.github.com>
2023-10-22 00:43:03 +0530

    Modified chart in MarketingAnalysis Page

--- Japan Lanka Japanese Language Institute/AdminDashControls/MarketingAnalysis.cs
+++ Japan Lanka Japanese Language Institute/AdminDashControls/MarketingAnalysis.cs
@@ -12,9 +12,37 @@ namespace Japan_Lanka_Japanese_Language_Institute.AdminDashControls
 {
     public partial class MarketingAnalysis : UserControl
     {
+        List<String> XValues1 = new List<String>();
+        List<int> YValues1 = new List<int>();
+
+        List<String> XValues2 = new List<String>();
+        List<int> YValues2 = new List<int>();
+
+        List<String> XValues3 = new List<String>();
+        List<int> YValues3 = new List<int>();
+
         public MarketingAnalysis()
         {
             InitializeComponent();
+            XValues1.Add("Ambassador 1");
+            YValues1.Add(42);
+
+            XValues1.Add("Ambassador 2");
+            YValues1.Add(61);
+
+            XValues1.Add("Ambassador 3");
+            YValues1.Add(50);
+
+            XValues1.Add("Ambassador 4");
+            YValues1.Add(40);
+
+            XValues1.Add("Ambassador 5");
+            YValues1.Add(52);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            chart1.Series["Enrollments"].Points.DataBindXY(XValues1, YValues1);
         }
     }
 }
